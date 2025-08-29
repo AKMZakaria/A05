930711f2d735013053e3172f0bda7232f9592990
AKMZakaria <akmzakaria488@gmail.com>
2025-08-29 22:15:45 +0600

    in-progress

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const historyData = [];
+
 let count = 0;
 document.getElementById('btn-heart-copy')
     .addEventListener('click', function (e) {
@@ -9,7 +11,7 @@ document.getElementById('btn-heart-copy')
     })
 
 
-function callBtnClick(id, msg) {
+function callBtnClick(id, msg, msg2) {
     document.getElementById(id)
         .addEventListener('click', function (e) {
 
@@ -26,12 +28,20 @@ function callBtnClick(id, msg) {
 
             }
             alert(msg)
+
+            const data = {
+                name1: msg2,
+                name11: 999,
+                date1: new Date().toLocaleTimeString()
+            }
+
+            historyData.push(data);
         })
 
 
 }
 
-callBtnClick('call1', '📞 Calling 999 - National Emergency')
+callBtnClick('call1', '📞 Calling 999 - National Emergency', 'Fire Service Number')
 callBtnClick('call2', '📞 Calling 999 - Police')
 callBtnClick('call3', '📞 Calling 999 - Fire Service')
 callBtnClick('call4', '📞 Calling 1994-999999 - Ambulance')
@@ -60,7 +70,7 @@ document.getElementById('btn-heart-copy')
 
 
 function copyNum(btnId, numId) {
-    document.getElementById(btnId).addEventListener('click', function() {
+    document.getElementById(btnId).addEventListener('click', function () {
         const text = document.getElementById(numId).innerText;
         navigator.clipboard.writeText(text);
         alert('Copied!');
@@ -76,5 +86,30 @@ copyNum('copy6', 'num6');
 copyNum('copy7', 'num7');
 copyNum('copy8', 'num8');
 copyNum('copy9', 'num9');
+
+
+
+
+
+
+const historyContainer = document.getElementById('history-container');
+historyContainer.innerText = '';
+
+for (data of historyData) {
+    const div = document.createElement('div');
+    div.innerHTML = `
+                <div>
+                    <h3 class="font-semibold">${data.name1}</h3>
+                    <p class="font-semibold text-[rgba(92,92,92,1)]">${data.name11}</p>
+                </div>
+                
+                <div>
+                    ${data.date1}
+                </div>
+            `
+
+    historyContainer.appendChild(div)
+}
+
 
 
